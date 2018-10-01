ae352bc527eef0dc6525d0215fa1ae9eb3b2f875
Ezekiel Kolawole <doneazy911@gmail.com>
2018-10-01 18:25:15 +0100

    update databese

--- config/database.php
+++ config/database.php
@@ -41,10 +41,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'BlogApp'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'eazyblog'),
+            'username' => env('DB_USERNAME', 'eazybright'),
             'password' => env('DB_PASSWORD', 'kolawole'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
