446b209ef9dfaaea80c6b3550acef514240cdb46
Ezekiel Kolawole <doneazy911@gmail.com>
2018-10-15 01:27:57 +0100

    changes

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'noreply@eazybright.com'),
-        'name' => env('MAIL_FROM_NAME', 'Eazybright'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@BlogApp.com'),
+        'name' => env('MAIL_FROM_NAME', 'BlogApp'),
     ],
 
     /*
